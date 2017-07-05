cd69dd356d16d2a1ede4e46de31a572a58d51d30
jhjaylim <jhjaylim@gmail.com>
2017-07-05 16:08:03 -0700

    Finish stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,14 +5,25 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function(value) {  
+    storage[someInstance.size()] = value;
   };
-
+  
   someInstance.pop = function() {
+    var result = storage[someInstance.size() - 1];
+    delete storage[someInstance.size() - 1];
+    return result;
   };
 
   someInstance.size = function() {
+    var count = 0;
+    for (var i in storage) {
+      count++;
+    }
+    return count;
   };
 
   return someInstance;
 };
+
+//var myStack = Stack(); // someInstance.push .... myStack.push;
