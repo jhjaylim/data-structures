428457aa04d7e1eee8d06cebe4db883e8ce7f81d
jhjaylim <jhjaylim@gmail.com>
2017-07-05 15:24:56 -0700

    Erase pre-existing code

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,30 +7,15 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    storage[someInstance.size().toString()]=value;
-  };
-
-  someInstance.dequeue = function() {
-    var result = storage['0'];
-    if (someInstance.size()!==0) {
-      for (var i = 0; i<someInstance.size(); i++) {
-        if (i===someInstance.size()-1) {
-          delete storage[i.toString()];
-        } else {
-          storage[i.toString()]= storage[(i+1).toString()];
-        }
-      }
-    }
-    return result;
     
   };
 
+  someInstance.dequeue = function() {
+   
+  };
+
   someInstance.size = function() {
-    var counter = 0;
-    for (var x in storage) {
-      counter++;
-    }
-    return counter;
+    
   };
 
   return someInstance;
