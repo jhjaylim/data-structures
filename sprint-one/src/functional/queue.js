d78dced1e43ad33dce5e636b1dbdbbb3c6aa8864
jhjaylim <jhjaylim@gmail.com>
2017-07-05 18:22:02 -0700

    Finish queue for functional class

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,15 +7,29 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function() {
-   
+    var result = storage[0]; 
+    console.log(storage[0]);
+    for (var i = 0; i < someInstance.size(); i++) {
+      if (i !== someInstance.size() - 1) {
+        storage[i] = storage[i + 1];
+        console.log(storage[i], '*', storage[i + 1]);
+      } 
+      delete storage[someInstance.size() - 1];
+    }
+    //delete storage[someInstance.size() - 1];
+    return result;
   };
 
   someInstance.size = function() {
-    
+    var count = 0;
+    for (var i in storage) {
+      count++;
+    }
+    return count;
   };
 
   return someInstance;
