928cc18015ba77e9155b317cb7fa58e1694e83c1
jhjaylim <jhjaylim@gmail.com>
2017-07-06 15:40:58 -0700

    Complete linked-list exercise

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,15 +4,51 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = Node(value);
+    
+    if (list.tail !== null) {
+      var previousTail = this.tail;
+      previousTail.next = newTail;
+    } else { // when the list is empty
+      this.head = newTail;
+    }
+    this.tail = newTail;
+     
   };
 
   list.removeHead = function() {
+
+    // define previous head
+    var previous = this.head;
+    // and new head
+    var next = this.head.next;
+    //second node to refer to head; previously current now not pointed to head;
+    this.head = next;
+    //return our previous head;
+    return previous.value;
+
   };
 
   list.contains = function(target) {
+    //find the head; 
+    var result = false;
+    var nodeToExam = this.head;
+    //while condition this value is not target, 
+    while (nodeToExam) {
+      // access next 
+      //if accessed result = true;
+      if (nodeToExam.value === target) {
+        return true; 
+      }
+      nodeToExam = nodeToExam.next;
+    }
+    return result;
+    
   };
+   
 
   return list;
+    
 };
 
 var Node = function(value) {
