6c94633c8e612de135e71c43547a3f858cedcf53
jhjaylim <jhjaylim@gmail.com>
2017-07-06 22:09:43 -0700

    Finish set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,36 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function(item) { // O(n) - linear
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  } 
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function(item) { // O(n) - linear
+  var result = false;
+  this._storage.forEach(function(val) {
+    if (val === item) {
+      result = true;
+    }
+  });
+  return result;
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function(item) { // O(n) - linear
+  var index;
+  this._storage.forEach(function(it, i) {
+    if (item === it) {
+      index = i;
+    }
+  });
+  this._storage.splice(index, 1);
+
 };
 
 /*
