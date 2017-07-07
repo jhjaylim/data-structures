16897f810ce6d59ad3444063ee3e903062fd7d1a
jhjaylim <jhjaylim@gmail.com>
2017-07-06 18:24:25 -0700

    Finish tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  // give newTree access to treeMethod
+  _.extend(newTree, treeMethods);
+ 
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,10 +14,39 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+
+  // access tree and children and push new tree to children
+  this.children.push(Tree(value));
+  
 };
 
 treeMethods.contains = function(target) {
+
+  // access tree itself and for each child find a value if that is equivalent
+  // recurse for children
+  // base case:
+  var result = false; 
+  if (this.value === target) {
+    result = true;
+  } 
+  // recursive case:
+  
+  _.each(this.children, function(child) {
+    
+    if (child.contains(target)) {
+      result = true; 
+    }
+    //true;
+    // somehow checks the child value and set result if necessary
+    // somewhere set result to true when child returns true;
+  });
+  
+  return result;
+
 };
+
+// somefunction();
+// this.contains(child)  
 
 
 
