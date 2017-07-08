b706934fa92e669a40badf833fc3b3aedcfdfea2
jhjaylim <jhjaylim@gmail.com>
2017-07-07 19:55:40 -0700

    Finish hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,19 +2,44 @@
 
 var HashTable = function() {
   this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+  this._storageContainer = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function(k, v) { // O(1) - constant
+  //input: key, value; output: none;
+  //constraints: simplified version; no tuples
+  //edge: same key, hashTable size: don't have to worry about these.
+  //take the key and translate it to variable: index;
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  //set the value to index location;
+  this._storageContainer.set(index, v, k);
+  
+  
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function(k) { // O(1) - constant
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //input: key; output: value at index position within storage
+  //access index position in storage;
+  //return what we find;
+  return this._storageContainer.get(index, k);
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function(k) { // O(1) - constant
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //input key; output none;
+  //edge case: we don't want to mututate indexing;
+  //empty the index position;
+  
+  // if we find that there is only one item in bucket, then set bucket to undefined
+    
+  this._storageContainer.set(index, undefined, k);  
+
+  // else iterate through buckey array, and remove tuple by splicing;
+
+
+
 };
 
 
--- sprint-two/src/hashTableHelpers.js
+++ sprint-two/src/hashTableHelpers.js
@@ -15,21 +15,61 @@ var LimitedArray = function(limit) {
   var storage = [];
 
   var limitedArray = {};
-  limitedArray.get = function(index) {
+  limitedArray.get = function(index, key) {// O(1) - statistically constant
     checkLimit(index);
-    return storage[index];
+    // find index of out storage : bucket
+    var bucket = storage[index];
+    // loop through each item in array
+    var result;
+    if (!bucket) {
+      return undefined;
+    } else {
+      for (var i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key ) {
+          result = bucket[i][1];  
+      
+        }
+      }
+    }
+    
+    
+    // if first item is a key, then return second item
+
+    return result;// value in tuple;
   };
-  limitedArray.set = function(index, value) {
+
+  limitedArray.set = function(index, value, key) { //O(1) statistically constant
     checkLimit(index);
-    storage[index] = value;
+    var bucket = storage[index];
+    if (value === undefined) { // removing case
+      if (bucket.length === 1) {
+        storage[index] = undefined;
+      } else {
+        var indexOfTupleToRemove;
+        for (var i = 0; i < bucket.length; i++) {
+          if (bucket[i][0] === key) {
+            indexOfTupleToRemove = i;
+          }
+        }
+        bucket[i] = undefined;
+      }
+    } else { // inserting case
+      if (!storage[index]) {
+        storage[index] = [];
+      } 
+      storage[index].push([key, value]);
+    } 
+          
+    
   };
-  limitedArray.each = function(callback) {
+
+  limitedArray.each = function(callback) { // O(n) - linear
     for (var i = 0; i < storage.length; i++) {
       callback(storage[i], i, storage);
     }
   };
 
-  var checkLimit = function(index) {
+  var checkLimit = function(index) { // O(1) constant  
     if (typeof index !== 'number') {
       throw new Error('setter requires a numeric index for its first argument');
     }
@@ -39,18 +79,21 @@ var LimitedArray = function(limit) {
   };
 
   return limitedArray;
+
 };
+
 
 // This is a "hashing function". You don't need to worry about it, just use it
 // to turn any string into an integer that is well-distributed between the
 // numbers 0 and `max`
-var getIndexBelowMaxForKey = function(str, max) {
+var getIndexBelowMaxForKey = function(str, max) { // O(1) - constant
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
     hash = (hash << 5) + hash + str.charCodeAt(i);
     hash = hash & hash; // Convert to 32bit integer
     hash = Math.abs(hash);
   }
+  
   return hash % max;
 };
 
